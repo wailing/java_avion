8b18ec69319454983a3796aaef31d85ae1af9423
anaisha <anais.ha@free.fr>
2015-12-30 17:34:03 +0100

    remove equipage now in vol, add exception

---
+++ affichageVols/Aeroport.java
@@ -0,0 +1,7 @@
+package affichageVols;
+
+/**
+ * Created by anais on 30/12/2015.
+ */
+public class Aeroport {
+}
---
+++ exception/InvariantBroken.java
@@ -0,0 +1,7 @@
+package exception;
+
+/**
+ * Created by anais on 30/12/2015.
+ */
+public class InvariantBroken {
+}
